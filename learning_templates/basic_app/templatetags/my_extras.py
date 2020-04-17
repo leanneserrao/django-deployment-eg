131eed5c1274a52973a1e490829a179424a87b4d
Leanne Serrao <leanne.serrao@yahoo.com>
2020-04-17 16:29:01 +0530

    'first'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,11 @@
+from django import relative_url_templates
+register = template.Library()
+
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+    this cuts out all values of "arg" from the string
+    """
+    return value.replace(arg,'')
+
+#register.filter('cut',cut)
